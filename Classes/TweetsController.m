22d47c931265d63de3d87f23a864e4b947c7b7ab
dbloete <mail@dennisbloete.de>
2009-09-22 22:50:43 +0200

    Better structure

--- Classes/TweetsController.m
+++ Classes/TweetsController.m
@@ -11,7 +11,6 @@
 - (void)parseTweetsSynchronously;
 - (void)parseTweetsWithCallback;
 - (void)parseTweetsInBackgroundThread;
-- (void)parseTweetsWithOperationQueue;
 @end
 
 
@@ -39,6 +38,10 @@
 	if (self.isLoading) return;
 	self.isLoading = YES;
 	
+	// -------------------------------------------
+	// Different approaches for parsing the tweets
+	// -------------------------------------------
+	
 	//[self parseTweetsSynchronously];
 	//[self parseTweetsWithCallback];
 	[self performSelectorInBackground:@selector(parseTweetsInBackgroundThread) withObject:nil];
@@ -54,73 +57,6 @@
 		self.tweets = result;
 		[self.tableView reloadData];
 	}
-}
-
-#pragma mark Various methods for parsing tweets
-
-// This methods loads tweets in a synchronous manner.
-// Too bad: Synchronous loading blocks the UI... this approach fails on app
-// launch, because all we see is a black screen until the tweets are loaded
-// and lazy loading the avatars leads to laggy table view scrolling.
-- (void)parseTweetsSynchronously {
-	NSData *data = [NSData dataWithContentsOfURL:self.connectionURL];
-    id result = [self tweetsFromJSONData:data];
-	[self loadedTweets:result];
-}
-
-// Here we are using the NSURLConnection delegate methods
-// This approach does not block the UI on app launch and reload, but is not
-// suitable either, because lazy loading the avatars still does not work.
-- (void)parseTweetsWithCallback {
-	NSURLRequest *request = [NSURLRequest requestWithURL:self.connectionURL];
-	NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
-	if (connection) receivedData = [[NSMutableData alloc] init];
-}
-
-// Using a background thread reduces the amount of code we have to write,
-// because we do not have to implement the callback methods.
-// Important: You need to define an autorelease pool for the spawned thread
-// and be sure to perform the result handler back on the main thread!
-- (void)parseTweetsInBackgroundThread {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	NSData *data = [NSData dataWithContentsOfURL:self.connectionURL];
-    id result = [self tweetsFromJSONData:data];
-	[self performSelectorOnMainThread:@selector(loadedTweets:) withObject:result waitUntilDone:YES];
-    [pool release];
-}
-
-// FIXME: Implement me please
-- (void)parseTweetsWithOperationQueue {
-
-}
-
-#pragma mark NSURLConnection callbacks
-
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    // this method is called when the server has determined that it
-    // has enough information to create the NSURLResponse
-    // it can be called multiple times, for example in the case of a
-    // redirect, so each time we reset the data.
-    [receivedData setLength:0];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-	[receivedData appendData:data];
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    [connection release];
-	id result = [self tweetsFromJSONData:receivedData];
-	[self loadedTweets:result];
-    [receivedData release];
-	receivedData = nil;
-}
-
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    [connection release];
-	[self loadedTweets:error];
-    [receivedData release];
-	receivedData = nil;
 }
 
 #pragma mark TableView
@@ -150,7 +86,7 @@
 - (NSURL *)connectionURL {
 	return [NSURL URLWithString:@"http://search.twitter.com/search.json?q=iphone&rpp=50"];
 }
-	
+
 - (void)setIsLoading:(BOOL)loading {
 	isLoading = loading;
 	if (isLoading) {
@@ -173,6 +109,72 @@
 		[tweetsArray addObject:tweet];
     }
 	return parseError ? (id)parseError : (id)tweetsArray;
+}
+
+#pragma mark -
+
+#pragma mark 1. Approach: Synchronous approach
+
+// This methods loads tweets in a synchronous manner.
+// Too bad: Synchronous loading blocks the UI... this approach fails on app
+// launch, because all we see is a black screen until the tweets are loaded
+// and lazy loading the avatars leads to laggy table view scrolling.
+- (void)parseTweetsSynchronously {
+	NSData *data = [NSData dataWithContentsOfURL:self.connectionURL];
+    id result = [self tweetsFromJSONData:data];
+	[self loadedTweets:result];
+}
+
+#pragma mark 2. Approach: Callback approach
+
+// Here we are using the NSURLConnection delegate methods
+// This approach does not block the UI on app launch and reload, but is not
+// suitable either, because lazy loading the avatars still does not work.
+- (void)parseTweetsWithCallback {
+	NSURLRequest *request = [NSURLRequest requestWithURL:self.connectionURL];
+	NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+	if (connection) receivedData = [[NSMutableData alloc] init];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    // this method is called when the server has determined that it
+    // has enough information to create the NSURLResponse
+    // it can be called multiple times, for example in the case of a
+    // redirect, so each time we reset the data.
+    [receivedData setLength:0];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+	[receivedData appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    [connection release];
+	id result = [self tweetsFromJSONData:receivedData];
+	[self loadedTweets:result];
+    [receivedData release];
+	receivedData = nil;
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    [connection release];
+	[self loadedTweets:error];
+    [receivedData release];
+	receivedData = nil;
+}
+
+#pragma mark 3. Approach: Background thread 
+
+// Using a background thread reduces the amount of code we have to write,
+// because we do not have to implement the callback methods.
+// Important: You need to define an autorelease pool for the spawned thread
+// and be sure to perform the result handler back on the main thread!
+- (void)parseTweetsInBackgroundThread {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	NSData *data = [NSData dataWithContentsOfURL:self.connectionURL];
+    id result = [self tweetsFromJSONData:data];
+	[self performSelectorOnMainThread:@selector(loadedTweets:) withObject:result waitUntilDone:YES];
+    [pool release];
 }
 
 @end
